431404270fae3016bef5f4e2ca2e06439bac7755
mahmudfoysal280@gmail.com <mahmudfoysal280@gmail.com>
2023-03-26 09:01:10 +0600

    components

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Country from "../Country/Country";
+import './Countires.css'
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => setCountries(data));
+
+  return (
+    <div>
+      <h1>Hello From Countires - {countries.length}</h1>
+      <div className="countries-cointainer">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca3}></Country>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Countries;
